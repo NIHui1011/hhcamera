40072ab9411c9b3b49d673296b0ad95c38bd27dc
NiDoubleHui <15958359737@163.com>
2024-11-21 13:55:36 +0800

    Initial Commit

---
+++ hhcamera/hhcameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  hhcameraApp.swift
+//  hhcamera
+//
+//  Created by nihuihui2 on 2024/11/21.
+//
+
+import SwiftUI
+
+@main
+struct hhcameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hhcameraTests/hhcameraTests.swift
@@ -0,0 +1,16 @@
+//
+//  hhcameraTests.swift
+//  hhcameraTests
+//
+//  Created by nihuihui2 on 2024/11/21.
+//
+
+import Testing
+
+struct hhcameraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
